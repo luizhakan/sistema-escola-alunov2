2c38bb04aaafb4909008ed2337c74beca7dba738
luizhakan <luizhakan2@protonmail.com>
2024-03-18 19:56:23 -0300

    build corrigido

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { addAluno, aluno, alunocpf, changeDateAluno } from "./functions/functions";
+import { addAluno, aluno, alunocpf, changeDateAluno } from "../functions/functions";
 
 const app = express();
 app.use(express.json());
